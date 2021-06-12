caacbdfb2b123714862f126dee5c7367884b875d
jane_glb <jane_glb@aliyun.com>
2021-06-12 22:55:31 +0800

    6.12 first version

---
+++ MyBatis_01_HelloWorld/src/com/fqy/mybatis/bean/Employee.java
@@ -0,0 +1,54 @@
+package com.fqy.mybatis.bean;
+
+/**
+ * @author fan_jennifer
+ * @create 2021-06-2021/6/12 17:45
+ */
+public class Employee {
+    private Integer id;
+    private  String lastName;
+    private  String email;
+    private  String gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
---
+++ MyBatis_01_HelloWorld/src/com/fqy/mybatis/test/MyBatisTest.java
@@ -0,0 +1,58 @@
+package com.fqy.mybatis.test;
+
+import com.fqy.mybatis.bean.Employee;
+import com.fqy.mybatis.dao.EmployeeMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author fan_jennifer
+ * @create 2021-06-2021/6/12 18:40
+ */
+public class MyBatisTest {
+    @Test
+    public void test() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession openSession = sqlSessionFactory.openSession();
+        Employee employee = openSession.selectOne("com.fqy.mybatis.EmployeeMapper.selectEmp", 1);
+        System.out.println("employee = " + employee);
+        
+
+    }
+
+    @Test
+    public void test1()  {
+        SqlSession sqlSession = null;
+        try {
+            //获取sqlsessionfactory对象
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory =
+                    new SqlSessionFactoryBuilder().build(inputStream);
+            sqlSession = sqlSessionFactory.openSession();
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+
+            Employee employee = mapper.getEmpById(1);
+            System.out.println(mapper.getClass());//class com.sun.proxy.$Proxy6,为接口自动创建代理对象
+            System.out.println("employee = " + employee);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+
+
+
+
+    }
+}
